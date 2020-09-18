f427fa383de987065a28a397c432f2f5c34fe151
David Beitey <david@davidjb.com>
2020-09-18 14:08:34 +0000

    Create steam.accept-agreement.user.js

---
+++ steam.accept-agreement.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name     Steam - Auto-accept agreement
+// @version  1.0
+// @grant    none
+// @match    https://store.steampowered.com/account/registerkey
+// ==/UserScript==
+
+document.getElementById('accept_ssa').checked = true

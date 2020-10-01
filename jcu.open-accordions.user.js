7c5214da2af705df412bfe85871b079b11e0ed64
David Beitey <david@davidjb.com>
2020-10-01 00:07:12 +0000

    Handle accordions on secure.jcu

--- jcu.open-accordions.user.js
+++ jcu.open-accordions.user.js
@@ -3,7 +3,12 @@
 // @version  1.0
 // @grant    none
 // @match    https://www.jcu.edu.au/*
+// @match    https://secure.jcu.edu.au/*
 // ==/UserScript==
 
+// www-style
 document.querySelectorAll('.accordion__link').forEach(e => e.click())
 document.querySelectorAll('.accordion-section .panel').forEach(e => e.className = '')
+
+// secure, uber-accordion
+document.querySelectorAll('.accordion__target').forEach(e => e.className = '')

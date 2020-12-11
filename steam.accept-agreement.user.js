a354e64d55dd1bed94982f0a088e847e29ceb355
David Beitey <david@davidjb.com>
2020-12-11 22:17:54 +0000

    Avoid error if element not found

--- steam.accept-agreement.user.js
+++ steam.accept-agreement.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name     Steam - Auto-accept agreement
-// @version  1.0
+// @version  1.0.1
 // @grant    none
 // @match    https://store.steampowered.com/account/registerkey
 // ==/UserScript==
 
-document.getElementById('accept_ssa').checked = true
+document.querySelectorAll('#accept_ssa').forEach(e => e.checked = true)

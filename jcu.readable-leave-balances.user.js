b463fbca7dca0182589919f3a8a753970a170e22
David Beitey <david@davidjb.com>
2021-04-28 14:43:06 +1000

    JCU: directly target frame content, remove timeout

--- jcu.readable-leave-balances.user.js
+++ jcu.readable-leave-balances.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         JCU - Readable Leave Balances
-// @version      1.1.0
+// @version      1.2.0
 // @description  Make HR system leave balances human-readable
 // @author       davidjb
 // @grant        none
@@ -10,49 +10,41 @@
 (function() {
   'use strict'
 
-  function main(frame) {
-    // Document becomes the frame's DOM
-    const rows = document.querySelectorAll('[summary="Leave Balances"] tr')
-    if (rows.length) {
-      frame.onLoad = main
-    }
-    rows
-      .forEach(row => {
-        if (row.children.length === 7) {
-          const type = row.children[0].textContent
-          const value = Number.parseFloat(row.children[4].textContent)
-          if (!isNaN(value)) {
-            const unit = row.children[6].textContent
-            const outputCell = document.createElement('td')
-            outputCell.style.fontFamily = 'Arial'
-            outputCell.style.fontWeight = 'bold'
-            outputCell.style.whiteSpace = 'nowrap'
-            outputCell.style.verticalAlign = 'middle'
+  const rows = document.querySelectorAll('[summary="Leave Balances"] tr')
+  rows
+    .forEach(row => {
+      if (row.children.length === 7) {
+        const type = row.children[0].textContent
+        const value = Number.parseFloat(row.children[4].textContent)
+        if (!isNaN(value)) {
+          const unit = row.children[6].textContent
+          const outputCell = document.createElement('td')
+          outputCell.style.fontFamily = 'Arial'
+          outputCell.style.fontWeight = 'bold'
+          outputCell.style.whiteSpace = 'nowrap'
+          outputCell.style.verticalAlign = 'middle'
 
-            if (unit.match(/hours/i)) {
-              outputCell.innerHTML = `${(value / 36.25).toFixed(1)} weeks (${(value / 7.25).toFixed(1)} days)`
-            } else if (unit.match(/days/i)) {
-              outputCell.innerHTML = `${(value / 5).toFixed(1)} weeks`
-            }
-            row.appendChild(outputCell)
+          if (unit.match(/hours/i)) {
+            outputCell.innerHTML = `${(value / 36.25).toFixed(1)} weeks (${(value / 7.25).toFixed(1)} days)`
+          } else if (unit.match(/days/i)) {
+            outputCell.innerHTML = `${(value / 5).toFixed(1)} weeks`
+          }
+          row.appendChild(outputCell)
 
-            const maximumCell = document.createElement('td')
-            maximumCell.style.fontFamily = 'Arial'
-            maximumCell.style.whiteSpace = 'nowrap'
-            maximumCell.style.verticalAlign = 'middle'
-            maximumCell.style.backgroundColor = 'pink'
-            if (type === 'Annual Leave') {
-              maximumCell.innerHTML = 'Max: 45 days'
-            } else if (type === 'Long Service Leave') {
-              maximumCell.innerHTML = 'Max: 15 weeks'
-            }
-            if (maximumCell.innerHTML) {
-              row.appendChild(maximumCell)
-            }
+          const maximumCell = document.createElement('td')
+          maximumCell.style.fontFamily = 'Arial'
+          maximumCell.style.whiteSpace = 'nowrap'
+          maximumCell.style.verticalAlign = 'middle'
+          maximumCell.style.backgroundColor = 'pink'
+          if (type === 'Annual Leave') {
+            maximumCell.innerHTML = 'Max: 45 days'
+          } else if (type === 'Long Service Leave') {
+            maximumCell.innerHTML = 'Max: 15 weeks'
+          }
+          if (maximumCell.innerHTML) {
+            row.appendChild(maximumCell)
           }
         }
-      })
-  }
-
-  window.setTimeout(() => document.querySelectorAll('iframe').forEach(main), 2000)
+      }
+    })
 })()

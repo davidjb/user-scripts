259b95647b0122e297bea8d147ecbae8d198e380
David Beitey <david@davidjb.com>
2021-04-30 09:22:53 +1000

    JCU: add download all link for docs

--- jcu.vacancy-display.user.js
+++ jcu.vacancy-display.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         JCU - Vacancy Display Helpers
-// @version      1.0.0
+// @version      1.1.0
 // @description  Add helpers to the vacancy display within MyHROnline
 // @author       davidjb
 // @grant        none
@@ -18,4 +18,24 @@
         row.children[1].innerHTML += ` (<a target="_blank" style="font-weight: bold;" href="https://myhronline.jcu.edu.au/ords/hrprodords/WK8227$VDC1.Startup?P_VACANCY_REF_NO=${vacancy}">Position Documents</a>)`
       }
     })
+
+  const applicantDocuments = document.querySelector('[summary="Attached Documents"]')
+  if (applicantDocuments) {
+    const downloadAll = document.createElement('a')
+    downloadAll.innerHTML = 'Download all documents'
+    downloadAll.href = '#'
+    downloadAll.style.fontFamily = 'Arial'
+    downloadAll.style.fontWeight = 'bold'
+    downloadAll.style.color = 'blue'
+    downloadAll.style.marginTop = '1rem'
+    downloadAll.style.marginBottom = '1rem'
+    downloadAll.onclick = () => {
+      document.querySelectorAll('[summary="Attached Documents"] a').forEach(link => {
+        link.target = '_blank'
+        link.rel = 'noreferrer noopener'
+        link.click()
+      })
+    }
+    applicantDocuments.after(downloadAll)
+  }
 })()
